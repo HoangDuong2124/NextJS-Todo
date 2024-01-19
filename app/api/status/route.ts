7186a2f7da7c192634b79d7e4d1387ea0bd7126a
HoangDuong2124 <103102495+HoangDuong2124@users.noreply.github.com>
2024-01-19 09:36:55 +0700

    Update

--- app/api/status/route.ts
+++
@@ -1,7 +0,0 @@
-import prisma from "@/lib/prisma"
-import { NextResponse } from "next/server"
-export async function GET() {
-    const res = await  prisma.status.findMany()
-    return Response.json(res)
-  }
- 
